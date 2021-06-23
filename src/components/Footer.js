3b218e7763ba1a5e4245aef18543843338a6f149
Lisa Barton <lisabarton23@yahoo.com>
2021-06-23 16:38:01 -0600

    added nav bar to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,19 @@ import React from 'react';
 
 const Footer = ()=>{
     return(
-        <nav className="navbar navbar-light bg-light">
-        <div className="container">
-            <a className="navbar-brand" href="#">FOOTER</a>
-        </div>
-        </nav>
+        <>
+         <h3 id="contactMe">Contact Me</h3>
+   <nav> <ul>
+        <li> 555-5555</li>
+        <li>lisabarton23@yahoo.com</li>
+        <li> <a href="https://github.com/lisabarton23">Git Hub</a></li>
+        <li> <a href="https://linkedin.com/in/lisa-barton-822830205">linkedin</a></li>
+    </ul>
+    <a href="#"></a>
+</nav> 
+
+        </>
+        
     )
 }
 
